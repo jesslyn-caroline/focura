1d02cee1fc2dedb75297b9e01b534aed6b6ee264
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-01-03 22:48:45 +0700

    Refactor code for better readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import { Poppins } from "next/font/google";
 // @ts-ignore
 import "./globals.css";
 
-const poppins = Poppins({
-  weight: ["400", "500", "600", "700"],});
+const poppins = Poppins({weight: ["400", "500", "600", "700"],});
 
   
 export default function RootLayout({
@@ -14,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
+        className={`${poppins.className} antialiased flex flex-col`}
       >
         {children}
       </body>
